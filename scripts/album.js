c718c4b6552e433d8623382d4ccf90ff106d35c2
nuelaonu <nuelaonu@yahoo.com>
2017-07-13 13:00:22 -0500

    checkpoint 12 completed

--- scripts/album.js
+++ scripts/album.js
@@ -30,13 +30,27 @@
      ]
  };
 
+ var albumPSquare = {
+     title: 'Testimony',
+     artist: 'Peter Smith',
+     label: 'SOP',
+     year: '1999',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Yellow', duration: '3:26' },
+         { title: 'Orange', duration: '3:00' },
+         { title: 'Green', duration: '4:01' },
+         { title: 'Blue', duration: '4:21'},
+         { title: 'Brown', duration: '3:15'}
+     ]
+ };
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
-        '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
-      + '  <td class="song-item-title">' + songName + '</td>'
-      + '  <td class="song-item-duration">' + songLength + '</td>'
-      + '</tr>'
+       '<tr class="album-view-song-item">'
+    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+    +   '  <td class="song-item-title">' + songName + '</td>'
+    +   '  <td class="song-item-duration">' + songLength + '</td>'
+    + '</tr>'
       ;
 
      return template;
@@ -64,6 +78,24 @@
      }
  };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+// Album button templates
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
  };
+
+      songListContainer.addEventListener('mouseover', function(event) {
+        // Only target individual song rows during event delegation
+      if (event.target.parentElement.className === 'album-view-song-item') {
+        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+      }
+      });
+      for (var i = 0; i < songRows.length; i++) {
+    songRows[i].addEventListener('mouseleave', function(event) {
+      // Selects first child element, which is the song-item-number element
+           this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    });
+}
